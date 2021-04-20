11854937240b4e537db36acf4f4b584f70105188
Paul Hendrickson <PaulRobHendrickson@gmail.com>
2021-04-20 14:25:42 -0500

    changed so images send correctly

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
       product: "V'nilla Almund Granola",
       description:
         "Every bag of Granola starts with us foraging for irresistible flavors and bountiful chunks of goodness. This light and crispy Granola is made with whole grain oats and crunchy Almunds.",
-      image: `http://localhost:${port}/01.jpg`,
+      image: `https://snack-server-nerdery.herokuapp.com/01.jpg`,
       votes: 0,
     },
     {
@@ -21,7 +21,7 @@ module.exports = {
       product: "Dill Pickle-Flavored Cashews",
       description:
         "If you like snacking on pickles, prepare to be obsessed with this snack: It combines the tangy flavor you love with the hearty crunch of cashews. And it's less drippy to eat on the go.",
-      image: `http://localhost:${port}/02.jpg`,
+      image: `https://snack-server-nerdery.herokuapp.com/02.jpg`,
       votes: 0,
     },
     {
@@ -30,7 +30,7 @@ module.exports = {
       product: "Honey Pecan Baklava Nutrition Bars",
       description:
         "This isn't your typical granola bar. Kashi's new plant-powered bars are a mix of grains, nuts, seeds, pea crisps and nut butter, and they come in four flavors: Dark Chocolate Cashew Chia, Honey Pecan Baklava, Peanut Hemp Crunch, Dark Chocolate and Nut Butter. The best part? Each one packs 8 grams of protein and 4 grams of fiber, so they'll fill you up from second breakfast to lunch.",
-      image: `http://localhost:${port}/03.jpg`,
+      image: `https://snack-server-nerdery.herokuapp.com/03.jpg`,
       votes: 0,
     },
     {
@@ -39,7 +39,7 @@ module.exports = {
       product: "Energy Bars",
       description:
         "Nutrition for sustained energy. Made with organic oats & soybeans. High in protein. No trans fats. 23 vitamins & minerals.",
-      image: `http://localhost:${port}/04.jpg`,
+      image: `https://snack-server-nerdery.herokuapp.com/04.jpg`,
       votes: 0,
     },
     {
@@ -48,7 +48,7 @@ module.exports = {
       product: "Crunchy Granola Bars",
       description:
         "Nature Valley Oats 'n Honey Granola Bars combine real honey and rolled oats to produce a sweet, wholesome snack.",
-      image: `http://localhost:${port}/05.jpg`,
+      image: `https://snack-server-nerdery.herokuapp.com/05.jpg`,
       votes: 0,
     },
     {
@@ -57,7 +57,7 @@ module.exports = {
       product: "Greek Yogurt",
       description:
         "Only natural ingredients. Grade A. Our authentic strained Greek yogurt is thick, creamy and an excellent source of protein.",
-      image: `http://localhost:${port}/06.jpg`,
+      image: `https://snack-server-nerdery.herokuapp.com/06.jpg`,
       votes: 0,
     },
   ],
